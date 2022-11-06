544a28e85a509c3f277497425ff13fdf28cf25b1
Xiangpeng <me@haoxp.xyz>
2022-11-06 16:31:51 -0600

    make clippy happy

--- src/base_node.rs
+++ src/base_node.rs
@@ -289,7 +289,7 @@ impl BaseNode {
     }
 
     #[inline]
-    pub(crate) fn read_lock<'a>(&'a self) -> Result<ReadGuard<'a>, ArtError> {
+    pub(crate) fn read_lock(&self) -> Result<ReadGuard, ArtError> {
         let version = self.type_version_lock_obsolete.load(Ordering::Acquire);
 
         // #[cfg(test)]
